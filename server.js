b8bff0ef739c660eac5b9b6fe6cf7a8d04620495
Graehm <gpatrellfazio@gmail.com>
2023-10-04 16:30:04 -0500

    seperation of concerns app and server although require dotenv is bugging out and crashing the app

---
+++ server.js
@@ -0,0 +1,10 @@
+// require('dotenv').config()
+// const PORT = process.env.PORT || 3009
+
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.resolve(path.join(__dirname, 'public', 'index.html')))
+// })
+
+// app.listen(PORT, () => {
+//     console.log(`API Listening on port ${PORT}`);
+// });
