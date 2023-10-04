86d7a33a70ff3ab7740a93f41ed2165356792e2a
Graehm <gpatrellfazio@gmail.com>
2023-10-04 16:24:35 -0500

    same movies api sans a backend and re written using React app

---
+++ src/App.js
@@ -0,0 +1,64 @@
+// require('dotenv').config()
+const PORT = process.env.PORT || 3009
+
+import { useEffect, useState } from "react";
+import Form from "./components/Form";
+import MovieDisplay from "./components/MovieDisplay";
+import MovieList from "./components/MovieList";
+import "./styles.css";
+import SearchForm from "./components/SearchForm";
+
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(path.join(__dirname, 'public', 'index.html')))
+})
+
+app.listen(PORT, () => {
+    console.log(`API Listening on port ${PORT}`);
+});
+
+
+export default function App() {
+  const apiKey = "98e3fb1f";
+
+  const [movie, setMovie] = useState(null);
+  const [movies, setMovies] = useState([]);  
+
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `https://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getMovies = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `https://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovies(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovie("Eraserhead");
+  }, []);
+
+  return (
+    <div className="App">
+      <Form getMovie={getMovie} />
+
+      <MovieDisplay movie={movie} />
+      <SearchForm getMovies={getMovies} />
+      <MovieList movies={movies} />
+    </div>
+  );
+}
